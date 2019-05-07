58b8c1fae25c2c252d362515b72e40b8d7388336
rogierlol <rap.rijsdijk@gmail.com>
2019-05-07 13:11:56 +0200

    dingen toegevoegd

--- BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/RegisterScreen.java
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/RegisterScreen.java
@@ -6,6 +6,6 @@ import javafx.scene.layout.Pane;
 public class RegisterScreen extends RegisterScreenUI {
 
     public RegisterScreen(Pane root) {
-        root.getChildren().addAll(registerButton, txtnaam);
+        root.getChildren().addAll(registerButton, txtnaam,txtstraatnaam,txtwoonplaats, txttelefoon,txthuisnummer, txtemail,txtpostcode, lblnaam,lblstraatnaam,lblwoonplaats,lbltelefoon,lblhuisnummer, lblemail,lblpostcode);
     }
 }
