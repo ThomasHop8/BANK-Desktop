44e2cd5685639a20f3ff27675d5803e638415f88
rogierlol <rap.rijsdijk@gmail.com>
2019-05-07 13:22:13 +0200

    jaja

--- BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/ui/RegisterScreenUI.java
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/ui/RegisterScreenUI.java
@@ -14,5 +14,29 @@ public abstract class RegisterScreenUI extends BaseScreen {
     @Override
     protected void setupMainUI() {
         registerButton = new Button("Registreren");
+        txtnaam = new TextField ("");
+        txtstraatnaam = new TextField ("");
+        txtwoonplaats = new TextField ("");
+        txttelefoon = new TextField ("");
+        txthuisnummer = new TextField ("");
+        txtemail = new TextField ("");
+        txtpostcode = new TextField ("");
+        lblnaam = new Label ("Naam en Achternaam");
+        lblstraatnaam = new Label ("Straatnaam");
+        lblwoonplaats = new Label ("Woonplaats");
+        lbltelefoon = new Label ("Telefoonnummer");
+        lblhuisnummer = new Label ("Huisnummer");
+        lblemail = new Label ("Email adres");
+        lblpostcode = new Label ("Postcode");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
