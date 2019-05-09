c9e08b87244334b33deb19d1e1c8e5fa0a8de5e1
kevintrouw1 <kam.trouw@ad-academie.nl>
2019-05-09 10:47:57 +0200

    Events toegevoegd aan de knoppen.

--- BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/EmployeeDashboard.java
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/EmployeeDashboard.java
@@ -6,9 +6,19 @@ import javafx.scene.layout.Pane;
 public class EmployeeDashboard extends EmployeeDashboardUI {
 
     public EmployeeDashboard(Pane root) {
+        particulierRegister.setOnAction(e ->{
+            System.out.println("Particuliere klant registeren");
+        });
+        bedrijfRegister.setOnAction(e ->{
+            System.out.println("Bedrijf registeren");
+        });
+        gemachtigdeRegister.setOnAction(e ->{
+            System.out.println("Gemachtigde registeren");
+        });
         logout.setOnAction(e ->{
             System.out.println("Je bent uitgelogd");
         });
+        
         
         root.getChildren().addAll(particulierRegister, bedrijfRegister, gemachtigdeRegister, logout, 
                 titleLabel, registerLabel, logoImageView, logoutImageView);
