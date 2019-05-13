616db4bb93bda749f7e4028cdf856f5c33443ebb
Chahine <chahinebm@gmail.com>
2019-05-13 16:34:43 +0200

    Transactie view.

--- BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/TransactionScreen.java
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/TransactionScreen.java
@@ -6,6 +6,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
@@ -28,6 +30,39 @@ public class TransactionScreen extends TransactionScreenUI {
         root.setCenter(gridCenter);
 
         /**
+         * Setup Colunms van TableView
+         */
+        // Kolom Datum
+        TableColumn<Transactie, String> datumColunm = new TableColumn<>("Datum");
+        datumColunm.setMinWidth(100);
+        datumColunm.setCellValueFactory(new PropertyValueFactory<>("datum"));
+
+        // Kolom Omschrijving
+        TableColumn<Transactie, String> omschrijvingColunm = new TableColumn<>("Omschrijving");
+        omschrijvingColunm.setMinWidth(200);
+        omschrijvingColunm.setCellValueFactory(new PropertyValueFactory<>("omschrijving"));
+
+        // Kolom bedragIn
+        TableColumn<Transactie, Double> bedragInColunm = new TableColumn<>("IN");
+        bedragInColunm.setMinWidth(100);
+        bedragInColunm.setCellValueFactory(new PropertyValueFactory<>("bedragIn"));
+
+        // Kolom bedragUit
+        TableColumn<Transactie, Double> bedragUitColunm = new TableColumn<>("UIT");
+        bedragUitColunm.setMinWidth(100);
+        bedragUitColunm.setCellValueFactory(new PropertyValueFactory<>("bedragUit"));
+
+        // Kolom Saldo
+        TableColumn<Transactie, Double> saldoColunm = new TableColumn<>("Saldo");
+        saldoColunm.setMinWidth(100);
+        saldoColunm.setCellValueFactory(new PropertyValueFactory<>("saldo"));
+
+        // Toevoegen in Table TableTransacties.
+        tableTransacties.setItems(getTransactie());
+        tableTransacties.getColumns().addAll(datumColunm,omschrijvingColunm,bedragInColunm,bedragUitColunm,saldoColunm);
+
+
+        /**
          * GridTop
          */
         // Settings
@@ -41,13 +76,22 @@ public class TransactionScreen extends TransactionScreenUI {
         /**
          * GridCenter
          */
+        // Settings
+        gridCenter.setAlignment(Pos.CENTER);
+        // Elementen toevoegen
+        gridCenter.add(lbTitel,0,0);
+        gridCenter.add(tableTransacties,0,1);
+
+
     }
 
     // All transactions
     public ObservableList<Transactie> getTransactie() {
         ObservableList<Transactie> transacties = FXCollections.observableArrayList();
-        transacties.add(new Transactie("13-5-2019", "Lening", 50.00, 0,5000) );
-        
-        return getTransactie();
+        transacties.add(new Transactie("13-5-2019", "Lening", 50.00, 0,5000) ); // todo remove dummy
+
+        // todo van database ophalen
+
+        return transacties;
     }
 }
