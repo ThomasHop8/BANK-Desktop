60bad45de4dd35cfca59a829acc8b1b20d9ab6e8
Thomas <tjp.hopstaken@ad-academie.nl>
2019-05-15 10:39:31 +0200

    :sparkles: Added classes

---
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/RepScreen.java
@@ -0,0 +1,11 @@
+package com.hoekbank.bank.desktop.screens;
+
+import com.hoekbank.bank.desktop.ui.RepScreenUI;
+import javafx.scene.layout.GridPane;
+
+public class RepScreen extends RepScreenUI {
+
+    public RepScreen(GridPane root) {
+
+    }
+}
---
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/ui/RepScreenUI.java
@@ -0,0 +1,11 @@
+package com.hoekbank.bank.desktop.ui;
+
+import com.hoekbank.bank.desktop.BaseScreen;
+
+public abstract class RepScreenUI extends BaseScreen {
+
+    @Override
+    protected void setupMainUI() {
+
+    }
+}
