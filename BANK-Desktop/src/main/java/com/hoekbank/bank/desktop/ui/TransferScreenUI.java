7299961302c819b2f24e84ba6437053acc809787
kevintrouw1 <kam.trouw@ad-academie.nl>
2019-05-20 11:56:10 +0200

    TransferScreen en TransferScreenUI aangemaakt.

---
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/ui/TransferScreenUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hoekbank.bank.desktop.ui;
+
+import com.hoekbank.bank.desktop.BaseScreen;
+
+/**
+ *
+ * @author kevin
+ */
+public abstract class TransferScreenUI extends BaseScreen {
+    
+    @Override
+    protected void setupMainUI() {
+        
+    }
+}
