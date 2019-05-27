71505bab0505c30a54211e3b21e9b07dffab67a5
rogierlol <rap.rijsdijk@gmail.com>
2019-05-27 12:24:05 +0200

    De ongebruikte inports zijn verwijderd en de grote lawine van witregels zijn geëlimineerd door Rogier

--- BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/UserOverview.java
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/screens/UserOverview.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.hoekbank.bank.desktop.api.API;
 import com.hoekbank.bank.desktop.api.APIService;
-import com.hoekbank.bank.desktop.enums.RegisterState;
 import com.hoekbank.bank.desktop.helpers.AppDataContainer;
 import com.hoekbank.bank.desktop.helpers.ScenesController;
 import com.hoekbank.bank.desktop.models.Rekening;
@@ -15,66 +14,43 @@ import com.sun.jersey.core.util.MultivaluedMapImpl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.stage.Popup;
-
 import javax.ws.rs.core.MultivaluedMap;
-
 /**
  *
  * @author kevin en rogier
  */
-
 public class UserOverview extends UserOverviewUI {
+
     public GridPane grid = new GridPane();
 
     public UserOverview(Pane root) {
-    tableRekeningen.setItems(getRekening());
-grid.setPadding(new Insets(10, 10, 10, 10));
-grid.setPrefSize(300, 300);
-grid.setVgap(5);
-grid.setHgap(5);
-spaarrekening.setText("Spaarrekening");
-grid.add(spaarrekening, 80, 95);
-grid.add(betaalrekening, 81, 95);
-grid.add(bankpas, 82, 95);
- 
+        tableRekeningen.setItems(getRekening());
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setPrefSize(300, 300);
+        grid.setVgap(5);
+        grid.setHgap(5);
+        spaarrekening.setText("Spaarrekening");
+        grid.add(spaarrekening, 80, 95);
+        grid.add(betaalrekening, 81, 95);
+        grid.add(bankpas, 82, 95);
+
         tableRekeningen.columnResizePolicyProperty();
 
-     
-            
-        tableRekeningen.widthProperty().addListener((source, oldWidth, newWidth)->{
+        tableRekeningen.widthProperty().addListener((source, oldWidth, newWidth) -> {
             TableHeaderRow header = (TableHeaderRow) tableRekeningen.lookup("TableHeaderRow");
-            header.reorderingProperty().addListener((observable, oldValue, newValue)-> header.setReordering(false));
+            header.reorderingProperty().addListener((observable, oldValue, newValue) -> header.setReordering(false));
         });
- 
-         addRekening.setOnAction(e ->{
-           
+
+        addRekening.setOnAction(e -> {
+
             root.getChildren().add(grid);
-             
-         });
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+        });
+
         transactions.setOnAction(e -> {
             String selectedRekNr = tableRekeningen.getSelectionModel().getSelectedItem().getRekeningnummer();
             selectedRekNr = selectedRekNr.replace(".", "");
@@ -83,7 +59,7 @@ grid.add(bankpas, 82, 95);
             new TransactionScreen(transPane, selectedRekNr);
             ScenesController.setStage(transPane);
         });
-        
+
         userOverviewPane.getChildren().addAll(logout, addRekening, transactions, titleLabel, rekeningLabel, addRekeningLabel, tableRekeningen, logoImageView, logoutImageView);
 
         pageContainer.getChildren().add(userOverviewPane);
@@ -100,16 +76,16 @@ grid.add(bankpas, 82, 95);
         return "REKENINGEN";
     }
 
-    private ObservableList<Rekening>getRekening() {
+    private ObservableList<Rekening> getRekening() {
         ObservableList<Rekening> rekeningen = FXCollections.observableArrayList();
 
         for (JsonElement rekening : getAccounts()) {
             JsonObject object = rekening.getAsJsonObject();
             String rekHouder;
 
-            if(!object.get("Volledige Naam").isJsonNull()) {
+            if (!object.get("Volledige Naam").isJsonNull()) {
                 rekHouder = object.get("Volledige Naam").getAsString();
-            } else if(!object.get("Bedrijfsnaam").isJsonNull()) {
+            } else if (!object.get("Bedrijfsnaam").isJsonNull()) {
                 rekHouder = object.get("Bedrijfsnaam").getAsString();
             } else {
                 continue;
