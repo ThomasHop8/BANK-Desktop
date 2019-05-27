a43463d445925a4c235734a9fa0ab3c9b5d6bd00
rogierlol <rap.rijsdijk@gmail.com>
2019-05-27 12:03:22 +0200

    credits naar je boy rogier ( de echte )

--- BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/helpers/Randompasswordgenerator.java
+++ BANK-Desktop/src/main/java/com/hoekbank/bank/desktop/helpers/Randompasswordgenerator.java
@@ -1,13 +1,15 @@
 package com.hoekbank.bank.desktop.helpers;
 import java.security.SecureRandom;
 import java.util.Random;
- 
+  //dit is gememaakt door Rogier Rijsdijk
 public class Randompasswordgenerator {
+        //Hier is gebruik gemaakt van de Random functie en een string die alle mogelijke getallen numbers en symbolen weergeeft die in het gegenereerde wachtwoord kunnen voorkomen.
     private static final Random RANDOM = new SecureRandom();
    private static final String GETALLETTERSYMBOOL = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*_=+-/";
   
     public static String generatePassword(int length) {
         StringBuilder returnValue = new StringBuilder(length);
+          //Hier staat de berekening van het gegenereerde wachtwoord. 
         for (int i = 0; i < length; i++) {
             returnValue.append(GETALLETTERSYMBOOL.charAt(RANDOM.nextInt(GETALLETTERSYMBOOL.length())));  
         }
